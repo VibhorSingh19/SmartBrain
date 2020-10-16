0f3e929c5e8d00525bcb1dc407428801643f100c
Vibhor Singh <vibhor.formal@gmail.com>
2020-10-16 16:44:46 +0530

    Nabar updated

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navigation() {
+  return (
+    <nav>
+         <p className='f3 link dim black underline pa3 pointer fr'>Sign out</p>  
+    </nav>
+  );
+}
+
+export default Navigation;
