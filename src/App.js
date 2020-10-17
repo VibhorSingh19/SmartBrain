5f82c086e80bc925c0e8227892aa04a1a3c679da
Vibhor Singh <vibhor.formal@gmail.com>
2020-10-17 13:28:01 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,58 +1,60 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/logo/logo';
 import SearchBox from './components/imageLinkerForm/imageLinkerForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
-//import Particles from "react-particles-js";
-//import { particlesOptions } from "./particlesOptions";
-const particleOption={
+import { particlesOptions } from "./ParticlesOptions";
+import Clarifai from 'clarifai';
 
-                   particles:{
-                    number:{
-                      value:30,
-                      density: {
-                        enable: true,
-                        value_area: 800
-                      }
-                    }
-                   }
-}
-function App() {
-  return (
-    <div className="App">
-      <Particles className="particles" 
-              params={{
-                particles: {
-                  number:{
-                      value:70,
-                      density: {
-                        enable: true,
-                        value_area: 800,
-                         line_linked: {
-                    shadow: {
-                     enable: true,
-                      color: "#3CA9D1",
-                      blur: 5
-                    }
-                  }
-                }
-                  }
-                }
-              }
-}
-      />
+const app = new Clarifai.App({
+ apiKey: '6744cbc8104940eba90468d428f3383f'
+});
+
+class App extends Component {
+ 
+  constructor(){
+    super();
+    this.state={
+
+      input:'',
+    }
+
+  }
+  onInputChange=(event) =>{
+   this.state.input=event.target.value;
+   console.log(this.state.input);
+
+  }
+  onButtonClick=()=>{
+
+    app.models.predict('6744cbc8104940eba90468d428f3383f',"https://www.uni-regensburg.de/Fakultaeten/phil_Fak_II/Psychologie/Psy_II/beautycheck/english/prototypen/w_sexy_gr.jpg").then(
+    function(response){
+     console.log(response);
+    },
+    function (err) {
+      // body...
+    }
+
+    );
+  }
+  render(){
+    return (
+    <div className="App" >
+
+     <Particles className="particles particles-box" params={particlesOptions} />
       <Navigation/>
       <Logo />
       <Rank/>
-      <SearchBox/>
+      <SearchBox onInputChange={this.onInputChange} onButtonClick={this.onButtonClick}/>
       {/*
-      
+       6744cbc8104940eba90468d428f3383f  
       <FaceRecognition>
     */}
     </div>
   );
+}
 }
 
 export default App;
--- src/components/imageLinkerForm/imageLinkerForm.js
+++ src/components/imageLinkerForm/imageLinkerForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './imageLinkerForm.css';
-function imageLinkerForm() {
+function imageLinkerForm({onInputChange,onButtonClick}) {
   return (
     <div className="App f3">
       <p>
@@ -8,8 +8,8 @@ function imageLinkerForm() {
       </p>
       <div className="center"> 
       <div className="center form pa4 br3 shadow-5">
-      <input className="f4 pa2 w-70 center" type="tex"/>
-      <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">Detect</button>
+      <input className="f4 pa2 w-70 center" type="tex" onChange={onInputChange}/>
+      <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple" onClick={onButtonClick}>Detect</button>
       </div>
       </div>
     </div>
