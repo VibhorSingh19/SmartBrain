5fbfe8ba1475087dc633fbb11bf193c17f1e58c3
Vibhor Singh <vibhor.formal@gmail.com>
2020-10-29 23:07:57 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -32,12 +32,15 @@ class App extends Component {
         entries:0,
         joined:''
       }
-    }
 
   }
  UpdateInfo=(data)=>{
  this.setState(user:{
-  id=data.io
+  id=data.id,
+  name=data.name,
+  email=data.email,
+  entries:data.entries,
+  joined:data.joined
  })
  }
   onInputChange=(event) =>{
